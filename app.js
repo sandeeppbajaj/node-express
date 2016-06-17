9253c7584a8d71f520cd034958ac2a9e18fa91f7
Sandeepp Bajaj <sandeepp.bajaj@gmail.com>
2016-06-17 10:01:30 -0700

    Add serve static dependency and using to serve static files

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+var serveStatic = require('serve-static');
 
 // Using the local logger module
 var logger = require('./logger.js');
 app.use(logger);
-
+app.use(serveStatic('public', {maxAge: 86400000}));
 //Binding static folder instead of writing routes for each public file
-app.use(express.static('public', {maxAge: 86400000}));
+//app.use(express.static('public', {maxAge: 86400000}));
 
 //Require the routes module for blocks
 var blocks = require('./routes/blocks');
